41897be01f234ce531926b4df273d6af8150ddb5
Gugor99 <121721730+Gugor99@users.noreply.github.com>
2023-12-15 14:44:58 +0100

    Added two inputs

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let inputOne = document.getElementById("input-one")
 let inputTwo = document.getElementById("input-two")
+let inputThree = document.getElementById("input-three")
+let inputFour = document.getElementById("input-four")
+
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
@@ -13,9 +16,16 @@ function generateRandomness(){
 function getIt(){
         realPsw = ""
         realPsw2 = ""
+        realPsw3 = ""
+        realPsw4 = ""
         for (let i = 0;i < passwordLength;i++){
         realPsw += generateRandomness()
         realPsw2 += generateRandomness()
-    } inputOne.textContent = realPsw
+        realPsw3 += generateRandomness()
+        realPsw4 += generateRandomness()
+    } 
+    inputOne.textContent = realPsw
     inputTwo.textContent = realPsw2
+    inputThree.textContent = realPsw3
+    inputFour.textContent = realPsw4
 }
